e8ac01a0822cbdf1ebbbd0155fe6cedc62b6e940
username=nam157 <nguyenhoainamhuongkhe2@gmail.com>
2023-02-07 21:12:10 +0700

    add api flask

--- api/api.py
+++ api/api.py
@@ -1,36 +1,15 @@
 import os
-import threading
-import time
-from queue import Empty, Queue
 
 import cv2
-import numpy as np
 from flask import Flask, request
 from obj_tracking import YOLOV5_SORT
 
 app = Flask(__name__)
-requestQueue = Queue()
+app.config["SAVE_IMAGE_UPLOAD"] = "static"
+app.config["SAVE_IMAGE_RESULT"] = "results"
 
 
-model = YOLOV5_SORT(weights="../saved_models/yolov5s.pt", img_size=(640, 640))
-
-
-def byte_to_image(file):
-    try:
-        file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        return opencv_image
-    except Exception:
-        return False
-
-
-def request_handler(data):
-    results = model.infer(source=data["image"])
-
-    return results
-
-
-threading.Thread(target=request_handler).start()
+model = YOLOV5_SORT(weights="../saved_models/yolov5s.pt", img_size=(640, 640), classes=0, tracking=False)
 
 
 @app.route("/isAlive", methods=["GET"])
@@ -40,14 +19,12 @@ def is_alive():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    file = request.files["image"]
-    img = byte_to_image(file=file)
-    data = {"image": img}
-    requestQueue.put(data)
-
-    response = {"filename": ""}
-
-    return str(data)
+    image = request.files["image"]
+    path_to_save = os.path.join(app.config["SAVE_IMAGE_UPLOAD"], image.filename)
+    image.save(path_to_save)
+    img_results = model.infer(path_to_save)
+    cv2.imwrite(os.path.join(app.config["SAVE_IMAGE_RESULT"], image.filename), img_results)
+    return str(img_results)
 
 
 if __name__ == "__main__":
--- api/obj_tracking.py
+++ api/obj_tracking.py
@@ -1,28 +1,97 @@
 import os
 import sys
 from pathlib import Path
+from typing import Tuple
 
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
+from draws import draw_boxes
 from models.common import DetectMultiBackend
 from tracker.sort import *
 from utils.dataloaders import LoadImages, LoadStreams
 from utils.general import check_img_size, increment_path, non_max_suppression, scale_coords
+from utils.plots import Annotator, colors
 
 
 class YOLOV5_SORT:
-    def __init__(self, weights, img_size):
+    def __init__(self, weights: str, img_size: Tuple[int], classes, tracking: bool = False):
         self.weights = weights
         self.img_size = img_size
-        pass
+        self.classes = classes
+        self.tracking = tracking
+        self.hide_labels = False
+        self.hide_conf = False
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = DetectMultiBackend(weights=self.weights, device=self.device)
+        if self.tracking:
+            self.sort_tracker = Sort(max_age=5, min_hits=2, iou_threshold=0.2)
 
-    def _load_model(self):
-        pass
-
-    def _dataloader(self):
-        pass
+    def _dataloader(self, source):
+        img_size = check_img_size(self.img_size, s=self.model.stride)
+        dataset = LoadImages(path=source, img_size=img_size)
+        return dataset
 
     def infer(self, source):
-        pass
+        minibatch = self._dataloader(source=source)
+        for path, img_1, img_2, vid_cap, string in minibatch:
+            img = torch.from_numpy(img_1).to(self.device).float()
+            img /= 255
+            if len(img.shape) == 3:
+                img = img[None]
+            pred = self.model(img, augment=False)
+            pred = non_max_suppression(pred, classes=self.classes, max_det=1000)
+            for i, det in enumerate(pred):
+                p, img_3, frame = path, img_2.copy(), getattr(minibatch.count, "frame", 0)
+                if len(det):
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img_3.shape).round()
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()
+                    if self.tracking:
+                        dets_to_sort = np.empty((0, 6))
+                        for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
+                            dets_to_sort = np.vstack((dets_to_sort, np.array([x1, y1, x2, y2, detclass])))
+                        tracked_dets = self.sort_tracker.update(dets_to_sort)
+                        tracks = self.sort_tracker.getTrackers()
+                        if len(tracked_dets) > 0:
+                            bbox_xyxy = tracked_dets[:, :4]
+                            identities = tracked_dets[:, 8]
+                            categories = tracked_dets[:, 4]
+                            draw_boxes(img_3, bbox_xyxy, identities, categories)
+                        for track in tracks:
+                            for i, _ in enumerate(track.centroidarr):
+                                if i < len(track.centroidarr) - 1:
+                                    cv2.line(
+                                        img_3,
+                                        (
+                                            int(track.centroidarr[i][0]),
+                                            int(track.centroidarr[i][1]),
+                                        ),
+                                        (
+                                            int(track.centroidarr[i + 1][0]),
+                                            int(track.centroidarr[i + 1][1]),
+                                        ),
+                                        (124, 32, 250),
+                                        thickness=3,
+                                    )
+                    else:
+                        annotator = Annotator(img_3, line_width=3, example=str(self.model.names))
+                        for *xyxy, conf, cls in reversed(det):
+                            c = int(cls)  # integer class
+                            label = (
+                                None
+                                if self.hide_labels
+                                else (self.model.names[c] if self.hide_conf else f"{self.model.names[c]} {conf:.2f}")
+                            )
+                            annotator.box_label(xyxy, label, color=colors(c, True))
+                        img = annotator.result()
+                return img_3
+
+
+if __name__ == "__main__":
+    inference = YOLOV5_SORT(weights="../saved_models/yolov5s.pt", img_size=(640, 640), classes=0, tracking=False)
+    import cv2
+
+    img = cv2.imread("G:/HN_LAP/yolov5_tracking/test_folder/1.jpg")
+    inference.infer(source=img)
